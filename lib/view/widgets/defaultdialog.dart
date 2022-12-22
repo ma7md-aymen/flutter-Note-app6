79b739efc4970a997fb5ce0a93568779e716d607
ma7md-ayman <ayman0code@gmail.com>
2022-12-22 21:24:45 +0200

    add button in the bottom sheet

--- lib/view/widgets/defaultdialog.dart
+++ lib/view/widgets/defaultdialog.dart
@@ -4,6 +4,7 @@ import 'package:noteapp6/view/widgets/textfiledshape.dart';
 
 void defaultDialog() => Get.bottomSheet(
       Container(
+        height: 350,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(24),
           color: Colors.grey[900],
@@ -16,17 +17,31 @@ void defaultDialog() => Get.bottomSheet(
             bottom: 20,
           ),
           child: Column(
-            children: const [
-              TextFild(
+            children: [
+              const TextFild(
                 hint: 'title',
                 maxlines: 1,
               ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFild(
+              const SizedBox(height: 20),
+              const TextFild(
                 hint: 'content',
                 maxlines: 5,
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: double.infinity,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.indigo[500],
+                ),
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Add',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
               ),
             ],
           ),
