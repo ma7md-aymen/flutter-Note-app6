f3c3d7b357a0a1d5bfc63f450e545d88cc0b0c4e
ma7md-ayman <ayman0code@gmail.com>
2022-12-22 16:55:46 +0200

    make some oop Things

--- lib/view/widgets/homebody.dart
+++ lib/view/widgets/homebody.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noteapp6/view/widgets/bodylisttile.dart';
 import 'package:noteapp6/view/widgets/homeappbar.dart';
 
 class HomeBody extends StatelessWidget {
@@ -14,53 +15,6 @@ class HomeBody extends StatelessWidget {
           HomeAppBar(),
           SizedBox(height: 25),
           BodyListTile(),
-        ],
-      ),
-    );
-  }
-}
-
-class BodyListTile extends StatelessWidget {
-  const BodyListTile({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 15, left: 10),
-      decoration: BoxDecoration(
-        color: Colors.indigo,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'const!',
-              style: TextStyle(fontSize: 22),
-            ),
-            subtitle: Text(
-              'Man i heat const it\'s my deathly word !',
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.white.withOpacity(.3),
-              ),
-            ),
-            trailing: const IconButton(
-              onPressed: null,
-              icon: Icon(Icons.remove_circle),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 29, bottom: 15),
-            child: Text(
-              'the date is',
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.white.withOpacity(.3),
-              ),
-            ),
-          ),
         ],
       ),
     );
